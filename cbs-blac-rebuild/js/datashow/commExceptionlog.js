c148b9b27278e60ebb8240168bea437203ad3080
yebin <yebin@bayax.cn>
2017-11-15 11:12:13 +0800

    。

--- cbs-blac-rebuild/js/datashow/commExceptionlog.js
+++ cbs-blac-rebuild/js/datashow/commExceptionlog.js
@@ -1,193 +1,211 @@
-$('#foot').load('public.html');
-$(function() {
-	publicHeadfun();
-	days();
-	
-	//时间选择改变
-	$("#slectFen ul li a").click(function() {
-		var loaddata = true;
-		$('span.txt').text($(this).text())
-		var value = $(this).attr("rel");
-		if (value == 1 || value == 2) {
-			$("#startTime").val($(this).attr('value'));
-			$("#endTime").val($(this).attr('value'));
-		} else if (value == 3 || value == 4 || value == 5) {
-			$("#startTime").val($(this).attr('value'));
-			$("#endTime").val(compareDate(1, 0));
-		} else if (value == 6 || value == 7 || value == 8) {
-			$("#startTime").val($(this).attr('value'));
-			if (value == 6) {
-				counts = 1;
-			} else if (value == 7) {
-				counts = 2;
-			} else if (value == 8) {
-				counts = 3;
-			}
-			$("#endTime").val(getCurrentMonthLastForSelect($(this).attr('value')));
-		} else if (value == 9) {
-			loaddata = false;
-			$("#startTime").val("");
-			$("#endTime").val("");
-		}
-		if (loaddata) {
-			commExcLoadData(1);
-		}
-	});
-	$("#slectFen ul li:nth-child(4) a").click();
+layui.use(['layer', 'element', 'laydate'], function() {
+    var layer = layui.layer;
+    var element = layui.element;
+    var laydate = layui.laydate;
+
+
+
+    var startime = laydate.render({
+        elem: '#startime',
+        theme: 'balck',
+        showBottom: false,
+        done: function(value, date, endDate) {
+            logVue.btnSelectTitle = '自定义';
+            logVue.startime = value;
+            $('#endtime').focus();
+
+            // console.log(value); //得到日期生成的值，如：2017-08-18
+            // console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
+            // console.log(endDate); //得结束的日期时间对象，开启范围选择（range: true）才会返回。对象成员同上。
+        }
+    });
+    var endtime = laydate.render({
+        elem: '#endtime',
+        theme: 'balck',
+        showBottom: false,
+        done: function(value, date, endDate) {
+            var st = logVue.startime;
+            var ed = logVue.endtime = value;
+            logVue.btnSelectTitle = '自定义';
+            // console.log('查看时间：' + warnVue.endtime);
+            if (st != '' && ed != '') {
+                if (bayaxCompareDate(st, ed, 1)) {
+
+                    if (compareYear(new Date(st), 10) <= value) {
+                        layer.msg("超过统计时间限制");
+                    } else {
+                        logVue.commExcLoadData(1);
+                    }
+                } else {
+                    logVue.endtime = '';
+                    layer.msg("结束时间需晚于开始时间");
+                }
+            }
+        }
+    });
+
+    var logVue = new Vue({
+        el: '#app',
+        data: {
+            project: {},
+            loadingShow: false,
+            showSecondDiv: false, //设备 子设备切换
+
+            // 下拉按钮数据
+            btnSelectTitle: '',
+            btnSelectdata: [],
+            startime: '',
+            endtime: '',
+            // 日志
+            commlogData: [],
+
+        },
+        mounted: function() {
+            var _this = this;
+            this.$nextTick(function() {
+                element.init();
+                bayaxInit();
+                this.btnSelectdata = dateData();
+
+                this.choiceTime(this.btnSelectdata[2]);
+
+            });
+
+        },
+        methods: {
+            // 下拉按钮选择时间
+            choiceTime: function(item) {
+                this.btnSelectTitle = item.name;
+                this.startime = item.startime;
+                this.endtime = item.endtime;
+
+                if (item.type) {
+                    this.commExcLoadData(1);
+                }
+                console.log(JSON.stringify(item, null, 2))
+            },
+            // 获取异常日志
+            commExcLoadData: function(page) {
+                var _this = this;
+                var dataUp = [
+                    'start_time=' + this.startime,
+                    'end_time=' + this.endtime + ' 23:59:59',
+                    'page=' + page,
+                    'page_item_count=13'
+                ];
+                console.log('上传的数据' + JSON.stringify(dataUp, null, 2));
+                $.ajax({
+                    url: apiurl + 'NodeLog/1?' + dataUp.join('&'),
+                    type: 'GET',
+                    beforeSend: function() {
+                        _this.loadingShow = true;
+                    },
+                    complete: function() {
+                        _this.loadingShow = false;
+                    },
+                    success: function(data) {
+                        if (data.success) {
+
+                            var datas = data.data.items;
+                            var arr = [];
+                            var length = 13;
+                            var nullData = {
+                                ip: '',
+                                name: '',
+                                status: '',
+                                create_time: '',
+                            }
+                            if (data.data == null || !Array.isArray(datas)) {
+                                layer.msg("无通讯状态信息");
+                                _this.commlogData = [];
+                                $(".tcdPageCode").hide();
+                                for (var i = 0; i < length; i++) {
+                                    _this.commlogData.push(nullData);
+                                }
+                            } else {
+                                var equDatas = datas.map(function(item) {
+                                    switch (Number(item.status)) {
+                                        case 1:
+                                            item.status = "正常";
+                                            break;
+                                        case 2:
+                                            item.status = "异常";
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    return item;
+                                });
+
+                                if (equDatas.length < length) {
+                                    var num = length - equDatas.length;
+                                    for (var i = 0; i < num; i++) {
+                                        equDatas.push(nullData);
+                                    }
+                                }
+                                _this.commlogData = equDatas;
+
+                                $(".tcdPageCode").show().createPage({
+                                    pageCount: data.data.pageCount,
+                                    current: page,
+                                    backFn: function(p) {
+
+                                        _this.commExcLoadData(p);
+                                    }
+                                });
+                            }
+                        } else {
+                            layer.msg(data.error_message)
+                        }
+                    },
+                    error: function(result) {
+
+                        layer.msg(result.error_message);
+                        returnLogIn(result);
+                    }
+                });
+
+            },
+
+
+
+            // 导出日志
+            exportWarnLog: function() {
+                var st = this.startime;
+                var ed = this.endtime;
+                if (st != '' && ed != '') {
+                    if (bayaxCompareDate(st, ed, 1)) {
+
+                        if (compareYear(new Date(st), 10) <= ed) {
+                            layer.msg("超过统计时间限制");
+                        } else {
+                            // 可以导出
+
+
+                            $.ajax({
+                                url: apiurl + 'nodeLog?start_time=' + st + '&end_time=' + ed + '&name=通信异常日志',
+                                type: 'PUT',
+                                success: function(data) {
+                                    if (data.success) {
+                                        window.location.href = '../' + data.data;
+                                    } else {
+                                        layer.msg(data.error_message);
+                                    }
+                                },
+                                error: function(data) {
+                                    layer.msg(data.error_message);
+                                    returnLogIn(data);
+                                }
+                            });
+                        }
+                    } else {
+                        layer.msg("结束时间需晚于开始时间");
+                    }
+                } else {
+                    layer.msg('起止时间不能为空');
+                }
+            }
+        }
+    });
+
 });
-
-$('#startTime').datepicker({
-	format: "yyyy-mm-dd ",
-	autoclose: true,
-	todayBtn: false,
-	pickerPosition: "bottom-right",
-	minView: 2
-}).on('changeDate', function(ev) {
-	$("#slectFen button .txt").html("自定义").attr('rel', 9);
-	$('#endTime')[0].focus();
-});
-$('#endTime').datepicker({
-	format: "yyyy-mm-dd ",
-	autoclose: true,
-	todayBtn: false,
-	pickerPosition: "bottom-right",
-	minView: 2
-}).on('changeDate', function(ev) {
-	$("#slectFen button .txt").html("自定义").attr('rel', 9);
-	timebeginisTrue = true;
-	var isYes = checkEndTime("startTime", "endTime", 1);
-	if (!isYes) {
-		$("#endTime").val("");
-		layer.msg("结束时间需晚于开始时间");
-		timebeginisTrue = false;
-	}
-	if (compareYear(new Date($('#startTime').val()), 10) <= ev.date.toLocaleDateString()) {
-		layer.msg("超过统计时间限制");
-		$("#endTime").val("");
-		timebeginisTrue = false;
-	}
-	if (timebeginisTrue) {
-		// 请求数据
-		commExcLoadData(1);
-	}
-
-});
-
-/**
- * [异常列表 数据请求]
- * @return {[type]} [description]
- */
-function commExcLoadData(page) {	
-	var dataUp = [
-	'start_time='+$('#startTime').val(),
-	'end_time='+$('#endTime').val()+' 23:59:59',
-	'page='+page,
-	'page_item_count=13'
-	];
-	console.log('上传的数据'+JSON.stringify(dataUp, null, 2));
-	$.ajax({
-		url: apiurl + 'NodeLog/1?'+dataUp.join('&'),
-		type: 'GET',	
-		beforeSend: function() {
-			$(".loading").show();
-		},
-		complete: function() {
-			$(".loading").hide();
-		},
-		success: function(result) {
-			$(".loading").hide();
-			if (result.success) {
-				console.log("报警数据:" + JSON.stringify(result, null, 2));
-
-				var data = result.data.items;
-				if (data == null) {
-					data = [];
-				}
-				for (var i in data) {
-					switch (data[i].status) {
-						case 1:
-							data[i].status = "正常";
-							break;
-						case 2:
-							data[i].status = "异常";
-							break;
-						
-					}
-				}
-				
-
-				
-				$('#commExclog-tbody tr td').html('');
-				$('#commExclog-tbody tr').each(function(i, element) {
-					if (i < data.length) {
-						var tds = '<td>' + data[i].name +
-							'</td><td>' + data[i].ip +
-							'</td><td >' + data[i].status +					
-							'</td><td >' + data[i].create_time +							
-							'</td>';
-						$(element).html(tds);
-					}
-				});
-
-				if (result.data.pageCount === 0) {
-					layer.msg('无数据');
-					$('.tcdPageCode').html('')
-				} else {
-					$('.tcdPageCode').createPage({
-						pageCount: result.data.pageCount,
-						current: page,
-						backFn: function(p) {
-							console.log("这是第" + p + "页");
-							commExcLoadData(p);
-						}
-					});
-				}
-
-			} else {
-				layer.msg(result.error_message);				
-			}
-		},
-		error: function(result) {
-			$(".loading").hide();
-			layer.msg(result.error_message);
-			returnLogIn(result);
-		}
-	});
-
-};
-
-/**
- * [导出用户操作日志]
- * @return {[type]} [description]
- */
-function exportUserOperationLog() {
-	var start = $('#startTime').val();
-	var end = $("#endTime").val()+' 23:59:59';
-	if (start !== '' && end !== '') {
-		if (start > end) {
-			layer.msg('开始时间大于结束时间');
-		} else if (compareYear(new Date(start), 10) <= end) {
-			layer.msg("超过统计时间限制");
-		} else {
-			$.ajax({
-				url: apiurl + 'nodeLog?start_time='+start+'&end_time='+end+'&name=通信异常日志',
-				type: 'PUT',
-				dataType: 'json',				
-				success: function(data) {
-					console.log(data.data)
-					if (data.success) {
-						window.location.href = '../'+data.data;
-					} else {
-						layer.msg(data.error_message);						
-					}
-				},
-				error: function(data) {
-					layer.msg(data.error_message);
-					returnLogIn(data);
-				}
-			});
-		}
-	} else {
-		layer.msg('时间区间不能为空');
-	}
-}
--- cbs-blac-rebuild/js/datashow/communicationException.js
+++ cbs-blac-rebuild/js/datashow/communicationException.js
@@ -1,3 +1,12 @@
+(function($) {
+    $(window).load(function() {
+        $(".div-equipment").mCustomScrollbar();
+        $(".div-sonequipment").mCustomScrollbar();
+    });
+})(jQuery);
+
+
+
 layui.use(['layer', 'element'], function() {
     var layer = layui.layer;
     var element = layui.element;
@@ -9,6 +18,11 @@ layui.use(['layer', 'element'], function() {
             loadingShow: false,
             showSecondDiv: false, //设备 子设备切换
             allEquipment: [], //所有设备
+            allSonEquipment: [], //当前设备 子设备
+            flag: {
+                type: 1,
+                id: ''
+            }
 
 
         },
@@ -17,149 +31,169 @@ layui.use(['layer', 'element'], function() {
             this.$nextTick(function() {
                 element.init();
                 this.getAllEquipmentData();
+
+                setInterval(function() {
+                    if (_this.flag.type === 1) {
+                        _this.getAllEquipmentData();
+                    } else {
+                        _this.commexcSonGetData(_this.flag.id);
+                    }
+                }, 10000)
             });
 
         },
         methods: {
+            // 获取设备通讯状态
             getAllEquipmentData: function() {
                 var _this = this;
-                var data = {
-                    "success": true,
-                    "data": {
-                        "pageCount": 0,
-                        "items": [{
-                                "name": "333",
-                                "ip": "333333333333",
-                                "status": 2,
-                                "id": 20,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "99",
-                                "ip": "999999999999",
-                                "status": 2,
-                                "id": 24,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "能耗01",
-                                "ip": "f01fafd74162",
-                                "status": 2,
-                                "id": 3,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "电梯01",
-                                "ip": "001122334455",
-                                "status": 2,
-                                "id": 6,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "test",
-                                "ip": "222222222222",
-                                "status": 2,
-                                "id": 7,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "能耗02",
-                                "ip": "123456789000",
-                                "status": 2,
-                                "id": 4,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "给排水02",
-                                "ip": "14cf92f95d01",
-                                "status": 2,
-                                "id": 2,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "给排水01",
-                                "ip": "123456789011~",
-                                "status": 1,
-                                "id": 15,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "测试点453452345234",
-                                "ip": "097654563456345",
-                                "status": 2,
-                                "id": 19,
-                                "create_time": "2017-11-01 15:10:27"
-                            },
-                            {
-                                "name": "报警01",
-                                "ip": "14cf92f96e46",
-                                "status": 1,
-                                "id": 5,
-                                "create_time": "2017-11-01 14:12:06"
-                            },
-                            {
-                                "name": null,
-                                "ip": null,
-                                "status": 1,
-                                "id": 1,
-                                "create_time": "2017-09-29 16:04:04"
-                            }
-                        ]
+
+
+                $.ajax({
+                    url: apiurl + 'nodelog/0',
+                    type: 'GET',
+                    beforeSend: function() {
+                        _this.loadingShow = true;
                     },
-                    "error_message": null
-                }
+                    complete: function() {
+                        _this.loadingShow = false;
+                    },
+                    success: function(data) {
+                        _this.loadingShow = false;
+                        if (data.success) {
 
-                if (data.success) {
-
-                    var datas = data.data.items;
-                    var arr = [];
-                    var length = 14;
-                    var nullData = {
-                        ip: '',
-                        name: '',
-                        status: '',
-                        create_time: '',
-                        iswarn: false,
-                        ishide: true
-                    }
-                    if (data.data == null || !Array.isArray(datas)) {
-                        layer.msg("无通讯状态信息");
-                        _this.allEquipment = [];
-                        for (var i = 0; i < length; i++) {
-                            _this.allEquipment.push(nullData);
-                        }
-                    } else {
-                        var equDatas = datas.map(function(item) {
-                            switch (Number(item.status)) {
-                                case 1:
-                                    item.status = "正常";
-                                    item.iswarn = false;
-                                    break;
-                                case 2:
-                                    item.status = "异常";
-                                    item.iswarn = true;
-                                    break;
-                                default:
-                                    break;
+                            var datas = data.data.items;
+                            var arr = [];
+                            var length = 14;
+                            var nullData = {
+                                ip: '',
+                                name: '',
+                                status: '',
+                                create_time: '',
+                                iswarn: false,
+                                ishide: true
                             }
-                            item.ishide = false;
-                            return item;
-                        });
+                            if (data.data == null || !Array.isArray(datas)) {
+                                layer.msg("无通讯状态信息");
+                                _this.allSonEquipment = [];
+                                for (var i = 0; i < length; i++) {
+                                    _this.allSonEquipment.push(nullData);
+                                }
+                            } else {
+                                var equDatas = datas.map(function(item) {
+                                    switch (Number(item.status)) {
+                                        case 1:
+                                            item.status = "正常";
+                                            item.iswarn = false;
+                                            break;
+                                        case 2:
+                                            item.status = "异常";
+                                            item.iswarn = true;
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    item.ishide = false;
+                                    return item;
+                                });
 
-                        if (equDatas.length < length) {
-                            var num = length - equDatas.length;
-                            for (var i = 0; i < num; i++) {
-                                equDatas.push(nullData);
+                                if (equDatas.length < length) {
+                                    var num = length - equDatas.length;
+                                    for (var i = 0; i < num; i++) {
+                                        equDatas.push(nullData);
+                                    }
+                                }
+                                _this.allEquipment = equDatas;
                             }
+                        } else {
+                            layer.msg(data.error_message)
                         }
-                        _this.allEquipment = equDatas;
-                    }
-                } else {
-                    layer.msg(data.error_message)
-                }
 
+                    },
+                    error: function(data) {
+                        publicAjaxError(data);
+                    }
+                });
+            },
+            // 获取子设备通讯状态
+            commexcSonGetData: function(id) {
+                this.showSecondDiv = true;
+                this.flag.type = 2;
+                this.flag.id = id;
+                var _this = this;
+                $.ajax({
+                    url: apiurl + 'taglog/1' + '?node_id=' + Number(id),
+                    type: 'GET',
+                    beforeSend: function() {
+                        _this.loadingShow = true;
+                    },
+                    complete: function() {
+                        _this.loadingShow = false;
+                    },
+                    success: function(data) {
+                        _this.loadingShow = false;
+                        if (data.success) {
+
+                            var datas = data.data.items;
+                            var arr = [];
+                            var length = 13;
+                            var nullData = {
+                                // ip: '',
+                                name: '',
+                                status: '',
+                                create_time: '',
+                                iswarn: false,
+                                ishide: true
+                            }
+                            if (data.data == null || !Array.isArray(datas)) {
+                                layer.msg("无通讯状态信息");
+                                _this.allSonEquipment = [];
+                                for (var i = 0; i < length; i++) {
+                                    _this.allSonEquipment.push(nullData);
+                                }
+                            } else {
+                                var equDatas = datas.map(function(item) {
+                                    switch (Number(item.status)) {
+                                        case 1:
+                                            item.status = "正常";
+                                            item.iswarn = false;
+                                            break;
+                                        case 2:
+                                            item.status = "异常";
+                                            item.iswarn = true;
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    item.ishide = false;
+                                    return item;
+                                });
+
+                                if (equDatas.length < length) {
+                                    var num = length - equDatas.length;
+                                    for (var i = 0; i < num; i++) {
+                                        equDatas.push(nullData);
+                                    }
+                                }
+                                _this.allSonEquipment = equDatas;
+                            }
+                        } else {
+                            layer.msg(data.error_message)
+                        }
+
+                    },
+                    error: function(data) {
+                        publicAjaxError(data);
+                    }
+                });
+
+
+            },
+            // 返回
+            btnGoBack: function() {
+                this.showSecondDiv = !this.showSecondDiv;
+                this.flag.type = 1;
+                this.flag.id = '';
             }
-
 
         }
     });
